caaf7195f22709f2bef3fb23a2d9d2815cba9e12
Ysiva@8689 <ysivaparvathi23@gmail.com>
2024-04-17 19:05:38 -0400

    first commit

---
+++ 4-12-24-Classwork-2.R
@@ -0,0 +1,22 @@
+# Fahrenheit temperatures
+fahrenheit <- c(45, 77, 20, 19, 101, 120, 212)
+
+# Conversion to Celsius (vectorized)
+celsius <- (fahrenheit - 32) * 5 / 9
+
+# Print the Celsius temperatures
+cat("Temperatures in Celsius:", celsius, "\n")
+
+# Highest Fahrenheit temperature
+highest_fahrenheit <- max(fahrenheit)
+
+# Lowest Fahrenheit temperature
+lowest_fahrenheit <- min(fahrenheit)
+
+# Average Fahrenheit temperature
+average_fahrenheit <- mean(fahrenheit)
+
+# Print results with descriptions
+cat("Highest Fahrenheit:", highest_fahrenheit, "\n")
+cat("Lowest Fahrenheit:", lowest_fahrenheit, "\n")
+cat("Average Fahrenheit:", average_fahrenheit, "\n")
